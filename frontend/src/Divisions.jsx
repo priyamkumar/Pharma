ee2574a497675bb5085d9d8aab5d3e01b92fde7f
PRIYAM <you@example.com>
2025-06-14 15:22:29 +0530

    changed divison links

--- frontend/src/Divisions.jsx
+++ frontend/src/Divisions.jsx
@@ -55,52 +55,52 @@ export default function Divisions() {
     {
       name: "CURE",
       description: "CLARED TRISTO",
-      image: `https://vibcaredivisions.s3.ap-south-1.amazonaws.com/2DivisionImage.png?X-Amz-Expires=259200&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXOEBKWXRBKCIXZQ/20250523/ap-south-1/s3/aws4_request&X-Amz-Date=20250523T135303Z&X-Amz-SignedHeaders=host&X-Amz-Signature=9861c66409c43c1777af992570cb9264bad9e62b450353e9ac38de93804f9b14`,
+      image: `https://vibcaredivisions.s3.ap-south-1.amazonaws.com/2DivisionImage.png?X-Amz-Expires=259200&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXOEBKWXRBKCIXZQ/20250614/ap-south-1/s3/aws4_request&X-Amz-Date=20250614T061709Z&X-Amz-SignedHeaders=host&X-Amz-Signature=2dba55ec74740826a37ff23df6faa74854d9eca2cc3b8af8f58017f7d096ff5b`,
     },
     {
       name: "GRACE",
       description: "GENERALIZED",
-      image: `https://vibcaredivisions.s3.ap-south-1.amazonaws.com/3DivisionImage.png?X-Amz-Expires=259200&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXOEBKWXRBKCIXZQ/20250523/ap-south-1/s3/aws4_request&X-Amz-Date=20250523T135303Z&X-Amz-SignedHeaders=host&X-Amz-Signature=e85c65819fc0a63b1af81e8c28eb7c0054796229795d215e0ba8379dadac56bf`,
+      image: `https://vibcaredivisions.s3.ap-south-1.amazonaws.com/3DivisionImage.png?X-Amz-Expires=259200&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXOEBKWXRBKCIXZQ/20250614/ap-south-1/s3/aws4_request&X-Amz-Date=20250614T061709Z&X-Amz-SignedHeaders=host&X-Amz-Signature=a0f603f42233f83152f82a76c4d116aeeff0064fe49df1e5da00c16f817cf997`,
     },
     {
       name: "MIND",
       description: "RADIO PRODUSTRY",
-      image: `https://vibcaredivisions.s3.ap-south-1.amazonaws.com/4DivisionImage.png?X-Amz-Expires=259200&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXOEBKWXRBKCIXZQ/20250523/ap-south-1/s3/aws4_request&X-Amz-Date=20250523T135303Z&X-Amz-SignedHeaders=host&X-Amz-Signature=60b51e7a84588b993de6c6bd778f59da2d46a240a8e8eed7ee904f3df91c6273`,
+      image: `https://vibcaredivisions.s3.ap-south-1.amazonaws.com/4DivisionImage.png?X-Amz-Expires=259200&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXOEBKWXRBKCIXZQ/20250614/ap-south-1/s3/aws4_request&X-Amz-Date=20250614T061709Z&X-Amz-SignedHeaders=host&X-Amz-Signature=33a76ffd107dfb960cd69b8f84b6b608167eb28491babd29e92903e4fb076837`,
     },
     {
       name: "PRIMA",
       description: "MENU",
-      image: `https://vibcaredivisions.s3.ap-south-1.amazonaws.com/5DivisionImage.png?X-Amz-Expires=259200&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXOEBKWXRBKCIXZQ/20250523/ap-south-1/s3/aws4_request&X-Amz-Date=20250523T135303Z&X-Amz-SignedHeaders=host&X-Amz-Signature=d53a6574c065399b97ab6c2eeece357b1aefa0755dc61a89903bd512c53149d8`,
+      image: `https://vibcaredivisions.s3.ap-south-1.amazonaws.com/5DivisionImage.png?X-Amz-Expires=259200&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXOEBKWXRBKCIXZQ/20250614/ap-south-1/s3/aws4_request&X-Amz-Date=20250614T061709Z&X-Amz-SignedHeaders=host&X-Amz-Signature=38bdf0e036b5471d68eabbe09d03589b4318ab39869712fbf534738d3baa8f91`,
     },
     {
       name: "OPTHO",
       description: "ORTHONOLOGY",
-      image: `https://vibcaredivisions.s3.ap-south-1.amazonaws.com/10DivisionImage.png?X-Amz-Expires=259200&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXOEBKWXRBKCIXZQ/20250523/ap-south-1/s3/aws4_request&X-Amz-Date=20250523T135303Z&X-Amz-SignedHeaders=host&X-Amz-Signature=2a8f1296a1c0e08f4b30e3aefeabf019f55f8af2b45b8a305d8eae74d1c19006`,
+      image: `https://vibcaredivisions.s3.ap-south-1.amazonaws.com/10DivisionImage.png?X-Amz-Expires=259200&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXOEBKWXRBKCIXZQ/20250614/ap-south-1/s3/aws4_request&X-Amz-Date=20250614T061709Z&X-Amz-SignedHeaders=host&X-Amz-Signature=de1e6aa6ecbe8fdc255629686cbfeedc3cd23520572b68149900996d179ac08b`,
     },
     {
       name: "NURALZ",
       description: "NURALZ",
-      image: `https://vibcaredivisions.s3.ap-south-1.amazonaws.com/11DivisionImage.png?X-Amz-Expires=259200&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXOEBKWXRBKCIXZQ/20250523/ap-south-1/s3/aws4_request&X-Amz-Date=20250523T135303Z&X-Amz-SignedHeaders=host&X-Amz-Signature=8bf81527993ea263f0eb9d209428b239010405e9779c81799f45bc2f7c8ba3a5`,
+      image: `https://vibcaredivisions.s3.ap-south-1.amazonaws.com/11DivisionImage.png?X-Amz-Expires=259200&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXOEBKWXRBKCIXZQ/20250614/ap-south-1/s3/aws4_request&X-Amz-Date=20250614T061709Z&X-Amz-SignedHeaders=host&X-Amz-Signature=36bef695980ebc9015c23932c6d88c5eca36a76e65f381472c7dba7ff7254c55`,
     },
     {
       name: "EVERMED",
       description: "",
-      image: `https://vibcaredivisions.s3.ap-south-1.amazonaws.com/1011DivisionImage.png?X-Amz-Expires=259200&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXOEBKWXRBKCIXZQ/20250523/ap-south-1/s3/aws4_request&X-Amz-Date=20250523T135303Z&X-Amz-SignedHeaders=host&X-Amz-Signature=db97028d26d8d593685e36b4a93134b336cd7538fe731ef5742c1a9f97c10428`,
+      image: `https://vibcaredivisions.s3.ap-south-1.amazonaws.com/1011DivisionImage.png?X-Amz-Expires=259200&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXOEBKWXRBKCIXZQ/20250614/ap-south-1/s3/aws4_request&X-Amz-Date=20250614T061709Z&X-Amz-SignedHeaders=host&X-Amz-Signature=ef3d16f69a43c4d218761d4ac34ebe5f29847ab0c93017cf8e3b3165f08208d5`,
     },
     {
       name: "GENVIMAX",
       description: "GENVIMAX",
-      image: `https://vibcaredivisions.s3.ap-south-1.amazonaws.com/1012DivisionImage.png?X-Amz-Expires=259200&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXOEBKWXRBKCIXZQ/20250523/ap-south-1/s3/aws4_request&X-Amz-Date=20250523T135303Z&X-Amz-SignedHeaders=host&X-Amz-Signature=8d7f3e3c6fa78a93261f25bcd5196fcd98dc091df49ee59f5ecda3d251222083`,
+      image: `https://vibcaredivisions.s3.ap-south-1.amazonaws.com/1012DivisionImage.png?X-Amz-Expires=259200&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXOEBKWXRBKCIXZQ/20250614/ap-south-1/s3/aws4_request&X-Amz-Date=20250614T061709Z&X-Amz-SignedHeaders=host&X-Amz-Signature=8c0139e71bd7cf8497ac3dbb5813ebb5ed14d8d105486ec3f836353510b096cc`,
     },
     {
       name: "MEDANOR",
       description: "MEDANOR",
-      image: `https://vibcaredivisions.s3.ap-south-1.amazonaws.com/1017DivisionImage.png?X-Amz-Expires=259200&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXOEBKWXRBKCIXZQ/20250523/ap-south-1/s3/aws4_request&X-Amz-Date=20250523T135303Z&X-Amz-SignedHeaders=host&X-Amz-Signature=161617e35287e51e141f81dc615d79fb835d3e46d0c9552d63d14b1d31089661`,
+      image: `https://vibcaredivisions.s3.ap-south-1.amazonaws.com/1017DivisionImage.png?X-Amz-Expires=259200&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXOEBKWXRBKCIXZQ/20250614/ap-south-1/s3/aws4_request&X-Amz-Date=20250614T061709Z&X-Amz-SignedHeaders=host&X-Amz-Signature=9466dfc6d8cf1f6875872a016feee8570564f9f9bfe2b982dee038c91bf61e03`,
     },
     {
       name: "VENTILA",
       description: "VENTILA",
-      image: `https://vibcaredivisions.s3.ap-south-1.amazonaws.com/1018DivisionImage.png?X-Amz-Expires=259200&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXOEBKWXRBKCIXZQ/20250523/ap-south-1/s3/aws4_request&X-Amz-Date=20250523T135303Z&X-Amz-SignedHeaders=host&X-Amz-Signature=4cda9df1ced8ee4d2976a69455444bcba9e6c842d6f21b00ad5b7f8c1b7c3571`,
+      image: `https://vibcaredivisions.s3.ap-south-1.amazonaws.com/1018DivisionImage.png?X-Amz-Expires=259200&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXOEBKWXRBKCIXZQ/20250614/ap-south-1/s3/aws4_request&X-Amz-Date=20250614T061709Z&X-Amz-SignedHeaders=host&X-Amz-Signature=c9646a0b90d27f6a84f8182dbcee92d13be1e1e0dd38924d79fca4b20bc05ad2`,
     },
   ];
 
